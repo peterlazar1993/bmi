9a1793ba9c250ff8be6945bb14afbd133d24c4df
Peter Lazar <peterlazar1993@gmail.com>
2021-04-21 02:51:02 +0530

    add custom RoundIconButton

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -145,3 +145,25 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
+
+class RoundIconButton extends StatelessWidget {
+  RoundIconButton({required this.onPressed, required this.icon});
+
+  final void Function() onPressed;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(icon),
+      elevation: 0,
+      constraints: BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      onPressed: onPressed,
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
+    );
+  }
+}
